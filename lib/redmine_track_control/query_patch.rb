a1ec1b7f18d636297c90e5bf5266c27c071d75de
fishermans <zu.mir@xyz.de>
2016-02-24 20:21:45 +0100

    fixed internal error on opening settings page

--- lib/redmine_track_control/query_patch.rb
+++ lib/redmine_track_control/query_patch.rb
@@ -15,11 +15,17 @@ module RedmineTrackControl
 
     module InstanceMethods
       def trackers_with_trackcontrol  
-        @trackers = []
-        project.children.visible.to_a.each do |prj|
-          @trackers |= Tracker.where(:id => RedmineTrackControl::TrackerHelper.valid_trackers_ids(prj, "show")).order("#{Tracker.table_name}.position")              
-        end             
-        @trackers = @trackers.compact.reject(&:blank?).uniq.sort  
+        unless project.nil?
+          @trackers = []
+          if project.children.nil?
+            @trackers ||= Tracker.where(:id => RedmineTrackControl::TrackerHelper.valid_trackers_ids(project,"show")).order("#{Tracker.table_name}.position")
+          else
+            project.children.visible.to_a.each do |prj|
+              @trackers |= Tracker.where(:id => RedmineTrackControl::TrackerHelper.valid_trackers_ids(prj, "show")).order("#{Tracker.table_name}.position")              
+            end             
+            @trackers = @trackers.compact.reject(&:blank?).uniq.sort  
+          end
+        end
       end
     end
   end
